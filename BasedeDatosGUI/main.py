6a27bf14d44bac49c23c4a576dd4387891411d5b
JordanAyuzo <aupj021014@gs.utm.mx>
2022-06-01 19:10:22 -0500

    modificacion de clase juego

--- BasedeDatosGUI/main.py
+++ BasedeDatosGUI/main.py
@@ -10,33 +10,19 @@ from grafica.error401 import *
 from grafica.error402 import *
 from grafica.error403 import *
 from PyQt5.QtWidgets import *
-
-
 class MiApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        """     AQUI SE PUEDE INSTANCIAR LA CLASE PARA LA 
-        CONEXION DE LA BASE DE DATOS
-        ejemplo:self.nombredeinstancia = clase.metodo()
-         """
-
-        """Secuencias de conexión a la base de datos mediante la case BaseDatos.
-            En dicha base se tienen ya registrados los datos para el inicio de sesión
-            por lo que están ocultos para el usuario final."""
-
-        """Finaliza secuencia de conexión"""
-
-        #self.setWindowFlag(Qt.FramelessWindowHint)
-        #self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
         self.ui.bjuego.clicked.connect(self.abrirJuego)
 
     def abrirJuego(self):
         self.hide()
         self.ventana = Juego()
         self.ventana.show()
-
 
 class Juego(QMainWindow):
     def __init__(self):
@@ -69,7 +55,7 @@ class Juego(QMainWindow):
             REGRESA en:datoB <Es una lista de 1 tupla>  ej:[(soft,dato2,dat3,dato4)](llega lista vacia 
             si no hay nada)
             datoB=self.clase.metodo_buscar(clave) """
-            datoB = [('Minecraft', 'dat1', 'dat2', 'dat3')]
+            datoB = [('Minecraft', 'dat1', 'dat2', 'dat3')]#cuando se haya hecho la conexion borrar esto
             #########################
             if datoB == []:
                 self.ui.cargando_0.setText('Cargando...')
@@ -105,59 +91,73 @@ class Juego(QMainWindow):
                     self.ui.cargando_0.setText('¡Operación Exitosa!')
                     time.sleep(0.2)
                     self.ui.progressBar_0.setValue(0)
-
     def modificar(self):
         clav = self.ui.lcod_2.text()
         clave = str(clav)
-        ########################
-        """Llama al metodo de busqueda pasandole como parametro la clave primaria
-        MANDA   en:clave <es la clave primaria>     ej:soft
-        REGRESA en:datoB <Es una lista de 1 tupla>  ej:[](llega lista vacia si no hay nada)
-        datoB=self.clase.metodo_buscar(clave) """
-        datoB = []
-        ########################
-        if datoB == []:
-            self.error(2)
+        if clave == '':
+            self.error(1)
         else:
-            nom = self.ui.jnombre_2.text()
-            nombre = str(nom)
-            clas = self.ui.jclasif_2.text()
-            clasificacion = str(clas)
-            desc = self.ui.jdesc_2.text()
-            descripcion = str(desc)
-            gen = self.ui.jgene_2.text()
-            genero = str(gen)
-            if nombre == '':
-                nombre = datoB[0][0]
-            if clasificacion == '':
-                clasificacion = datoB[0][1]
-            if descripcion == '':
-                descripcion = datoB[0][2]
-            if genero == '':
-                genero = datoB[0][3]
-            ################################
-            """Llama al metodo de actualizar pasandole como parametros:
-                   el nombre,clasificacion,descripcion y el genero
-            MANDA   en:nombre,clasificacion,descripcion,genero
-            REGRESA en:valor true o false
-            valor=self.clase.metodo_actualizar(nombre,clasificacion,descripcion,genero) """
-            valor = True
-            ###############################
-            if valor != True:
-                self.ui.cargando_2.setText('Cargando...')
-                for i in range(0, 50):
-                    time.sleep(0.02)
+            ########################
+            """Llama al metodo de busqueda pasandole como parametro la clave primaria
+            MANDA   en:clave <es la clave primaria>     ej:soft
+            REGRESA en:datoB <Es una lista de 1 tupla>  ej:[](llega lista vacia si no hay nada)
+            datoB=self.clase.metodo_buscar(clave) """
+            datoB = [('Minecraft', 'dat1', 'dat2', 'dat3')]  #cuando ya este enlazado borrar esto
+            ########################
+            if datoB == []:
+                for i in range(0,   41):
+                    time.sleep(0.01)
                     self.ui.progressBar_2.setValue(i)
-                self.ui.cargando_2.setText('')
-                self.error(3)
+                self.error(2)
+                time.sleep(0.2)
                 self.ui.progressBar_2.setValue(0)
             else:
-                self.ui.cargando_2.setText('Cargando...')
-                for i in range(0, 101):
-                    time.sleep(0.02)
-                    self.ui.progressBar_2.setValue(i)
-                self.ui.cargando_2.setText('¡Operación Exitosa!')
-
+                nom = self.ui.jnombre_2.text()
+                nombre = str(nom)
+                clas = self.ui.jclasif_2.text()
+                clasificacion = str(clas)
+                desc = self.ui.jdesc_2.text()
+                descripcion = str(desc)
+                gen = self.ui.jgene_2.text()
+                genero = str(gen)
+                if nombre == '':
+                    nombre = datoB[0][0]
+                if clasificacion == '':
+                    clasificacion = datoB[0][1]
+                if descripcion == '':
+                    descripcion = datoB[0][2]
+                if genero == '':
+                    genero = datoB[0][3]
+                ################################
+                """Llama al metodo de actualizar pasandole como parametros:
+                    el nombre,clasificacion,descripcion y el genero
+                MANDA   en:nombre,clasificacion,descripcion,genero
+                REGRESA en:valor true o false
+                valor=self.clase.metodo_actualizar(nombre,clasificacion,descripcion,genero) """
+                valor = True   #cuando ya estee enlazado borrar esto
+                ###############################
+                if valor != True:
+                    self.ui.cargando_2.setText('Cargando...')
+                    for i in range(0, 50):
+                        time.sleep(0.02)
+                        self.ui.progressBar_2.setValue(i)
+                    self.ui.cargando_2.setText('')
+                    self.error(3)
+                    time.sleep(0.2)
+                    self.ui.progressBar_2.setValue(0)
+                else:
+                    self.ui.cargando_2.setText('Cargando...')
+                    for i in range(0, 101):
+                        time.sleep(0.01)
+                        self.ui.progressBar_2.setValue(i)
+                    self.ui.cargando_2.setText('¡Operación Exitosa!')
+                    time.sleep(0.2)
+                    self.ui.progressBar_2.setValue(0)
+                    self.ui.lcod_2.setText('')
+                    self.ui.jnombre_2.setText('')
+                    self.ui.jclasif_2.setText('')
+                    self.ui.jdesc_2.setText('')
+                    self.ui.jgene_2.setText('')
     def crear(self):
         nom = self.ui.jnombre.text()
         nombre = str(nom)
@@ -182,18 +182,24 @@ class Juego(QMainWindow):
             if respuesta != True:
                 self.ui.cargando.setText('Cargando...')
                 for i in range(0, 50):
-                    time.sleep(0.02)
+                    time.sleep(0.01)
                     self.ui.progressBar.setValue(i)
                 self.ui.cargando.setText('')
                 self.error(3)
+                time.sleep(0.2)
                 self.ui.progressBar_2.setValue(0)
             else:
                 self.ui.cargando.setText('Cargando...')
                 for i in range(0, 101):
-                    time.sleep(0.02)
+                    time.sleep(0.01)
                     self.ui.progressBar.setValue(i)
-                self.ui.cargando.setText('¡Operaciónn Exitosa!')
-
+                self.ui.cargando.setText('¡Operación Exitosa!')
+                time.sleep(0.2)
+                self.ui.progressBar_2.setValue(0)
+                self.ui.jnombre.setText('')
+                self.ui.jclasif.setText('')
+                self.ui.jdesc.setText('')
+                self.ui.jgene.setText('')
     def buscar(self):
         clav = self.ui.lcod.text()
         clave = str(clav)
@@ -205,7 +211,6 @@ class Juego(QMainWindow):
             MANDA   en:clave <es la clave primaria>     ej:soft
             REGRESA en:datoB <Es una lista de 1 tupla>  ej:[](llega lista vacia si no hay nada)
             datoB=self.clase.metodo_buscar(clave) """
-
             print(type(clave))
             datosB = self.principal.consultaDatos('juego', clave, self.cursor)
 
@@ -223,7 +228,6 @@ class Juego(QMainWindow):
                     self.ui.tableWidget.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row[2]))
                     self.ui.tableWidget.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(row[3]))
                     tablerow += 1
-
     ##NO TOCAR ESTOS ELEMENTOS##
     def retroceder(self):
         self.hide()
